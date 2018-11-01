f04cc1a81cb6a08a3f62a421646e7f42ec59ba90
Lucas Alexandre <lucas.alexandre.df@gmail.com>
2018-11-01 18:02:34 -0300

    forms

--- icf/icf/urls.py
+++ icf/icf/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core.views import home
 from core.views import charts
 from core.views import forms
