d9ec6b58ac6504f611e69260b69d8a0bf446066a
João Pedro Soares <jpcirqueira81@gmail.com>
2018-11-30 01:05:47 -0200

    banco

--- icf/core/models.py
+++ icf/core/models.py
@@ -13,3 +13,17 @@ class UserProfileInfo(models.Model):
 
 def __str__(self):
   return self.user.username
+
+class Usuario(models.Model):
+
+	nome = models.CharField(max_length = 35)
+
+	conhecimento_java = models.BooleanField()
+
+	conhecimento_python = models.BooleanField()
+
+	conhecimento_aeromodelismo = models.BooleanField()
+
+	conhecimento_fenomenos = models.BooleanField()
+
+	id = models.IntegerField(primary_key = True)
--- icf/core/views.py
+++ icf/core/views.py
@@ -12,7 +12,61 @@ from django.views.decorators.http import require_POST
 import random
 import datetime
 import time
+from core.models import Usuario
 
+def num_java():
+    usuarios = Usuario.objects.all().values('conhecimento_java')
+    c =0
+    i=0
+    for i in usuarios:
+        if  True:
+            c = c + 1
+    return c  
+
+c = num_java()    
+
+def num_python():
+    usuarios = Usuario.objects.all().values('conhecimento_python')
+    d = 0
+    i=0
+    for i in usuarios:
+        if True:
+            d = d + 1
+    return d
+
+d = num_python()
+
+def num_aero():
+    usuarios = Usuario.objects.all().values('conhecimento_aeromodelismo')
+    e = 0
+    i=0
+    for i in usuarios:
+        if True:
+            e = e + 1
+    return e
+    
+e = num_aero()
+
+def num_fenomenos():
+    usuarios = Usuario.objects.all().values('conhecimento_fenomenos')
+    f = 0
+    i=0
+    for i in usuarios:
+        if True:
+            f = f + 1
+    return f
+
+f = num_fenomenos()
+
+def num_pessoas():
+    usuarios = Usuario.objects.all().values('id')
+    g = 0
+    for i in usuarios:
+        if True:
+            g = g + 1
+    return g
+
+g =num_pessoas()
 
 def search(request):
     user_list = User.objects.all()
@@ -23,7 +77,7 @@ def home(request):
     return render(request,'index.html')
 
 def homeProfessor(request):
-    return render(request,'homeProfessor.html')
+    return render(request,'homeProfessor.html',{'numero_java': c, 'numero_python':d, 'numero_aero':e , 'numero_fenomenos':f, 'numero_pessoas':g})
 
 def charts(request):
     return render(request,'charts.html')
@@ -111,3 +165,4 @@ def user_login(request):
 
     else:
         return render(request, 'login.html', {})
+
